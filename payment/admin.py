052fe52000b33afb6557219580606be6f2f3a401
LisaOz <frolovae9@gmail.com>
2025-11-03 15:23:47 +0000

    Registered payments in the admin site

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Payment
 
 # Register your models here.
+
+
+"""
+To display payments in Django admin
+"""
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('booking', 'amount', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
