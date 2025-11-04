12e1717e050181cdab103fe911586806517bcc5b
LisaOz <frolovae9@gmail.com>
2025-11-04 15:03:56 +0000

    Implemented sending confirmation email after the successful booking & payment. Debugged the error message in the booking_failed template

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,13 @@
 from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
 from django.shortcuts import render, get_object_or_404, redirect
+from django.template.loader import render_to_string
+
 from getaway.models import Booking
 from .models import Payment
 import stripe
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -10,8 +15,15 @@ import stripe
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
+"""
+Vies for payment in the process with all details
+"""
+
 def payment_create(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id)
+
+    # Save booking ID in session to access it after payment success
+    request.session['booking_id'] = booking.id
 
     # Get the price from the Trip related to this Booking
     trip_price = booking.trip.trip.price or 0
@@ -40,12 +52,46 @@ def payment_create(request, booking_id):
     return redirect(checkout_session.url)
 
 
-
+"""
+View for successfully completed payment with the confirmation email with all detaild
+"""
 def payment_completed(request):
-    return render(request, "payment/completed.html", {
-        'redirect_url': 'getaway:home'  # or 'getaway:home'
+    booking_id = request.session.get('booking_id')
+    if booking_id:
+        booking = Booking.objects.get(id=booking_id)
+
+        # Send confirmation email
+        send_confirmation_email(booking)
+
+        # Clear booking_id from session
+        del request.session['booking_id']
+
+    return render(request, "payment/completed.html")
+
+
+"""
+View for confirmation email after successful payment
+"""
+def send_confirmation_email(booking):
+    subject = f"Booking Confirmation - ID {booking.id}"
+    message = render_to_string('emails/booking_confirmation.txt', {
+        'booking': booking,
+        'total_amount': booking.total_amount,
     })
 
+    email = EmailMessage(
+        subject,
+        message,
+        'GreenGetaway@example.com',  # From
+        [booking.email],             # To
+    )
 
+    # Send email saved in txt file
+    email.send()
+
+
+"""
+View for cancelled booking
+"""
 def payment_cancel(request):
     return render(request, "payment/cancel.html")
